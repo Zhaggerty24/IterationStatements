99a2e971c0e478692dfd0f3c269f273997219095
Zachary Haggerty <zhaggerty24@gmail.com>
2022-09-04 15:06:21 -0400

    initial commit

---
+++ IterationStatements/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IterationStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //will print out 0-14
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+        }
+    }
+}
